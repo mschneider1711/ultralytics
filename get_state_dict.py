61f402375fb735a192285b8a3f582e1e4ef8775c
Marc Schneider <92093026+mschneider1711@users.noreply.github.com>
2025-10-16 20:10:19 +0200

    delete unwanted scripts

--- get_state_dict.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("/Users/marcschneider/Desktop/MasterArbeit_Experimente/yolov8s_modular_experiments/yolov8s_modular_experiments/run2/yolov8_P3SwinTrafoCSP_run2/weights/best.pt")
-for i, layer in enumerate(model.model.model):
-    print(f"Layer {i}: {layer.__class__.__name__}")
-
-
-print(model.model.model[6])
